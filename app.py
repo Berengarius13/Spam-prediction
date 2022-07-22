60f87e136d820d3f4fa3271a2e1bb2bcf50bbef2
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-22 07:20:18 +0530

    Index completed

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import logic
 
 app = Flask(__name__)
 
-
+# Route is the url requested by user
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template("index.html")
 
-@app.route('/sub', methods = ['POST'])
+
+@app.route('/sub', methods=['POST'])
 def submit():
-    if request.method == "POST" :
+    if request.method == "POST":
         strr = request.form["strings"]
         lis = [strr]
-        print(logic.predict_spam(lis))
-    return render_template("index.html", n = strr)
+        prediction = logic.predict_spam(lis)
+        print(prediction[0])
+        return render_template("index.html", n=prediction)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
